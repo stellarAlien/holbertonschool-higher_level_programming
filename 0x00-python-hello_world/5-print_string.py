de1fae047f1fa46ec596a892b20273b2bb29d31e
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-20 21:18:13 +0100

    5

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 str = "Holberton School"
-for j in range (3):
-    print(str, end ='')
+for j in range(3):
+    print(str, end='')
 print('\n')
-for i in range(0, 9):                            
+for i in range(0, 9):
     print(str[i], end='')
-print('\n')
