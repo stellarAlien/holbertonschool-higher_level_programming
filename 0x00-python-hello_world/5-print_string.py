449ed3d03a5c910392de9453677dab0d840d74de
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-20 20:54:01 +0100

    first commit

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Holberton School"
+for j in range (3):
+    print(str, end ='')
+print('\n')
+for i in range(0, 9):                            
+    print(str[i], end='')
+print('\n')
---
+++ 0x00-python-hello_world/Readme.md
@@ -0,0 +1,3 @@
+#General 
+this is the first python project and it  generally follows the structure of the previous  learning experience
+#
