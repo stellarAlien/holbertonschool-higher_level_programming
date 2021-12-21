607ce94dbb951b20c8c3fc7ca123bfdfba751fcc
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-21 20:58:22 +0100

    3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(97,123):
+for i in range(97, 123):
     if (i == 113 or i == 101):
         pass
     else:
-        print(chr(i), end = '')
+        print("{:s}".format(chr(i)), end='')
