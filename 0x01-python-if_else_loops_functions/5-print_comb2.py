da66d4415114211c8458f6d268247f89c56195c8
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-21 21:20:49 +0100

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(0, 10):
-        print("{0:d}{1:d}".format(i, j), end ='')
+        print("{0:d}{1:d}".format(i, j), end='')
         if(i == 9 and j == 9):
-            break;
-        print("{:s}".format(', '), end= '')
-
+            break
+        print("{:s}".format(', '), end='')
