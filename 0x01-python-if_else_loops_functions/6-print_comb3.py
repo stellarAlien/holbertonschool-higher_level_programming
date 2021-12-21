c9bc1af46d11e2acdb8923252437ee4a1fbf668d
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-21 21:27:14 +0100

    6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 9):
+    for j in range(i+1, 10):
+        print("{:d}{:d}".format(i, j),end='')
+        if(i == 8 and j == 9):
+            break
+        print("{:s}".format(', '),end='')
