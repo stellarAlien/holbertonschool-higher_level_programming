532696ac902bc9b99ece5d9af3fd4cc34b902aae
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-23 22:46:50 +0100

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-if __name__ =="__main__":
+if __name__ == "__main__":
     import os
     i = 1
-    l = len(sys.argv) - 1
-    if(l == 1):
+    c = len(sys.argv) - 1
+    if(c == 1):
         print("{}arguments.".format(0))
-    elif(l == 2):
+    elif(c == 2):
         print("{} argument:".format(1))
-        print("{}: {}".format(i, sys.argv[i])
+        print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("{} arguments:".format(l))
+        print("{} arguments:".format(c))
         while i in range l:
             print("{}: {}".format(i, sys.argv[i]))
