35ef0ef9ecc4b98ea5e994c378259ce95e3b9a2c
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-23 22:15:43 +0100

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,13 @@
 if __name__ =="__main__":
     import os
     i = 1
-    if(sys.argc == 1):
+    l = len(sys.argv) - 1
+    if(l == 1):
         print("{}arguments.".format(0))
-    elif(sys.argc ==2):
-        print("{}argument:".format(1))
+    elif(l == 2):
+        print("{} argument:".format(1))
         print("{}: {}".format(i, sys.argv[i])
     else:
-        print("{} arguments:".format(sys.argc))
-        while(sys.argv[i]):
+        print("{} arguments:".format(l))
+        while i in range l:
             print("{}: {}".format(i, sys.argv[i]))
