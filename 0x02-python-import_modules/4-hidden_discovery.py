ed70e7914791b923aa98ec9896c37049229e2953
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2021-12-23 23:56:26 +0100

    another version of 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    for n in dir(hidden_4):
-        if n[:2] != "__":
-            print(n)
+    length = dir(hidden_4)
+    for i in range(len(length)):
+        if(length[i][0] != '_'):
+            print("{}".format(length[i]))
