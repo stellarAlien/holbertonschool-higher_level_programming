242286eb53f93310377fd6bae897c8f66823b550
root <root@LAPTOP-8L71RL06.localdomain>
2021-12-23 23:51:33 +0100

    4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+    import re
+    import sys
+    import hidden_4.pyc
+    names = dir(hiiden_4)
+    for n in names
+    for i in (97, 122):
+        if((re.match("[^__]{2}[\w]{1,}", n) and n[0] == chr(i)):
+                print("{:s}".format(n))
+        
