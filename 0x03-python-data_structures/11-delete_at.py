21f251739068633a5ed1f3a63d2514116ad161f3
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-03 23:55:05 +0100

    11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-    return (0, None)
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
     if (idx >= len(my_list) or idx < 0):
