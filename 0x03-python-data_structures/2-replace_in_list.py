2a12e6c270dfcd7aa7338b137b8aa055a98ea039
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-03 12:55:10 +0100

    2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if(idx < 0 or idx > len(my_list)i - 1):
+    if(idx < 0 or idx > len(my_list) - 1):
         return(None)
-    my_list[idx - 1] = element
-    return(0)
+    else:
+        my_list[idx] = element
