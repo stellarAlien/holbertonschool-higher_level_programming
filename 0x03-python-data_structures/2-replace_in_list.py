37cd86dba4384819677accd478b719e05def9ff5
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-03 12:43:41 +0100

    2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if(idx < 0 or idx > len(my_list)):
+    if(idx < 0 or idx > len(my_list)i - 1):
         return(None)
-    my_list[idx] = element
+    my_list[idx - 1] = element
+    return(0)
