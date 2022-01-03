dd18b95ba588c9b4bed568204049560fa13dd00e
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-03 23:51:44 +0100

    11

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+    return (0, None)
+    else:
+        return (len(sentence), sentence[0])
+    if (idx >= len(my_list) or idx < 0):
+        return my_list
+    else:
+        del my_list[idx]
+    return my_list
