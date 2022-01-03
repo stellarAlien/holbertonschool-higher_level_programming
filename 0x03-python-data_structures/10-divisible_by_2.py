251271ac7e3db7f8139de6294b1670d70e12b44f
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-03 23:34:09 +0100

    10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,8 +5,6 @@ def divisible_by_2(my_list=[]):
     for i in my_list:
         if (i % 2 == 0):
             vlist[k++] = True
-            print("{:d} is divisible by 2".format(i))
         else:
-            print("{:d} is not  divisible by 2".format(i))
             vlist[k++] = False
     return(vlist)
