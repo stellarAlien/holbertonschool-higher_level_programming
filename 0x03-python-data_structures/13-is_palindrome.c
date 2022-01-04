e20cd320ea383759b681f57d2cf665a0848aa792
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-04 22:32:59 +0100

    13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stddef.h>
+/**
+ * is_palindrome - determines if a linked list is a palindrome list
+ * @head : pointer to head of list
+ * Return: 1 if palindrome else 0
+ */
+int is_palindrome(listint_t **head)
+{
+	int i = 0, j, arr[256];
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	if (*head == NULL)
+	{
+		return (0);
+	}
+	if (*head->next == NULL)
+	{
+		return (1);
+	}
+	listint_t *p = (listint_t *)malloc(sizeof(listint_t))
+
+	while (*p)
+	{
+		p = p->next;
+		arr[i++] = p->n;
+	}
+	for (j = 0; j < (i / 2); j++)
+	{
+		if (arr[i] != arr[i - j - 1])
+			return (0)
+	}
+	free(p);
+	return (1);
+}
+
