e7524f75bc38ce20b011dd2fca134bcf8d7442fa
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-04 17:42:11 +0100

    7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    df = tuple(map(sum, zip(tuple_a, tuple_b)))
-    print(df)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
