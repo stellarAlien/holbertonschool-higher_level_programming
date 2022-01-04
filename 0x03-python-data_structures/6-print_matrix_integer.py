623c1058b2bffecf1a8de39704558e3cecf3a814
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-04 14:28:28 +0100

    m6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,12 @@ def print_matrix_integer(matrix=[[]]):
     if not matrix:
         return matrix
     for i in matrix:
+        le = len(i)
+        k = 0
         for j in i:
-            print("{:d}".format(j),end = ' ')
+            if (k == le - 1):
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+                k += 1
         print()
