9d174c5399daa074d5b9473d70e711c04fcb4caa
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-05 21:56:53 +0100

    7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if(not a_dictionary):
-        return(None)
-    if(key in a_dictionary):
-        a_dictionary[key] = value
-    else:
-        a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
     return(a_dictionary)
-    
