db057577d94073b655a5267e08b11342e76e1008
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-05 21:59:08 +0100

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (not my_list or (not search) or (not replace)):
-        return(my_list)
-    ls = list(my_list)
-    for i in ls[0:len(ls) - 1]:
-        if (ls[i] == search):
-            ls[i] = replace
-    return(ls)
+    if my_list is not None:
+        return([x if x != search else replace for x in my_list])
+    return Non
