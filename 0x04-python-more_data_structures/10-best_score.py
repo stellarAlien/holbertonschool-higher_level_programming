c83bd69a3125d1712cca128a3b3afd1f08bf3296
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-05 21:41:30 +0100

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if(a_dictionary):
-        m = max(a_dictionary, key = lambda k: a_dictionary[k])
+        m = max(a_dictionary, key=lambda k: a_dictionary[k])
         return(m)
     return(None)
-
-
