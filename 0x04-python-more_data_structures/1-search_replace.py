e34ed17dc813d678c346e26bb6f2833870e846df
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-05 18:17:32 +0100

    1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if (not my_list):
+    if (not my_list or (not search) or (not replace)):
         return(my_list)
     ls = list(my_list)
     for i in ls[0:len(ls) - 1]:
