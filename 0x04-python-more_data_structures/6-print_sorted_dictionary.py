8d9810f4d4436500a46fbb3dab68afd76c785871
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-05 20:56:45 +0100

    6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if (not key):
-        return(a_dictionary)
-    if(key in a_dictionary):
-        a_dictionary.pop(key)
-    return(a_dictionary)
+def print_sorted_dictionary(a_dictionary):
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
