241819a968214b06d609a22e12d7e37716aff3b5
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-13 12:09:51 +0100

    101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin//python3
 def square_matrix_map(matrix=[]):
-    if(matrix is not None):
-        new = list()
-    for i in matrix:
-        new.append(list(map(lambda x: x**2, i)))
-    return(new)
+    return(list(map(lambda x: (list(map(lambda y: y**2, x))), matrix)))
