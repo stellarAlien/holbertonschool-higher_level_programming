98cc649bb8e2b273af8861cb953ef6073f23b2e6
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-17 21:19:03 +0100

    4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    n = list()
+    x = 0
+    for i in range(0, list_length - 1):
+        try:
+            x = my_list_1[i]/my_list_2[i]
+        except (TypeError, ValueError):
+            print("wrong type")
+            x = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            x = 0
+        except IndexError:
+            print("out of range")
+            x = 0
+            return
+        except:
+            x = 0
+        finally:
+            n.append(x)
+    return(n)
