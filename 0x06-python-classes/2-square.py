db73d4d37a785c2eb1361d93fe5efbf6c94ea930
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-18 20:03:52 +0100

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,7 +6,7 @@ class Square:
     """ initializes size value and verifies it"""
     def __init__(self, size=0):
         self.__size = size
-        if(type(size) == str):
+        if(type(size) != int):
             raise TypeError("size must be an integer")
         if (not int(size) >= 0):
             raise ValueError("size must be >= 0")
