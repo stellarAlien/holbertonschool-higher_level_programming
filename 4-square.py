fe4b1a9fd22177cf460d47c71e0a457e357ff904
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-18 21:35:37 +0100

    4

---
+++ 4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module 4-square class Square """
+
+
+class Square:
+    """ initializes size value and verifies it"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if(type(size) == str):
+            raise TypeError("size must be an integer")
+        if (not int(size) >= 0):
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    def area(self):
+        """ returns square area"""
+
+        return(self.__size**2)
