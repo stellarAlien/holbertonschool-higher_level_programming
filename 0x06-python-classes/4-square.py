f6ae7a780232ef13b938ea606739e319be384183
stellarAlien <91025540+stellarAlien@users.noreply.github.com>
2022-01-18 23:30:28 +0100

    ran git init in ubuntu so i  have a problem push

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Module 4-square class Square """
+
+
+class Square:
+    """ initializes size value and verifies it"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size(self, value):
+        if(type(value) != int):
+            raise TypeError("size must be an integer")
+        if (not int(size) >= 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def size(self):
+        """returns size"""
+        return self.__size
+
+    def area(self):
+        """ returns square area"""
+
+        return(self.__size**2)
