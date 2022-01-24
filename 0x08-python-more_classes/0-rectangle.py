37d245d7a84effe68806abf9310d7fc52e4438ac
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-24 19:00:52 +0100

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/bin/bash/python3
+"""module defines a rectangle"""
+
+
+class Rectangle{
+        """rectangle class that has rectangle properties"""
+        def __init__(self, t, w, x, y):
+        """initilaize a rectangle"""
+        self.t, self.w, self.x, self.y = 0
+        }
