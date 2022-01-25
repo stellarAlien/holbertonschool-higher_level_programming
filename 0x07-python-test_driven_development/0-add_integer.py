00369d628e1ac641208a282d9412069423f98b73
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-25 17:34:26 +0100

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,16 +3,18 @@
  Module def add_integer(a, b=98)
  adds two floats and returns an integer
  """
- def add_integer(a, b=98):
-     """
-     adds two flaots and returns strictly an integer
-     no need to worry about overflows 
-     as a google search indicated that only numpy and pan\
-             das have c-style fixed precision integers
-     """
-     if(not isinstance(a, int) or not isinstance(a, float)):
-         raise TypeError("a must be an integer")
-     if(not isinstance(b, int) or not isinstance(b, float)):
-         raise TypeError("b must be an integer")
-     x = int(a +b)
-     return x
+
+
+def add_integer(a, b=98):
+    """
+    adds two flaots and returns strictly an integer
+    no need to worry about overflows
+    as a google search indicated that only numpy and pan\
+            das have c-style fixed precision integers
+    """
+    if(not isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    if(not isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+    x = int(a + b)
+    return x
