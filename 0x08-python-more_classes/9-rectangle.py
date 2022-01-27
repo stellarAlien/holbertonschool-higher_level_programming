710857d210cc918e4e833592f96cd91494ea1cd1
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-27 23:21:47 +0100

    9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,6 +93,6 @@ class Rectangle:
         del(rect_2)
 
     @classmethod
-    def square(cls, size = 0):
+    def square(cls, size=0):
         """  returns a square """
         return cls(size, size)
