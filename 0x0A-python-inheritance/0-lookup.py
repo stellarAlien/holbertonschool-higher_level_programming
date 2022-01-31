7b5c4ba960f2511d6dde67d1774049a4100ea891
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-31 15:23:44 +0100

    Readme and 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+module def lookup(obj):
+returns list of all attributes and method in a class
+"""
+def lookup(obj):
+    """ return elements of an objects"""
+    ls = dir(obj)
+    return(list(ls))
