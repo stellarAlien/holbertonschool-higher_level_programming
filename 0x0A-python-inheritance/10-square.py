b812425b35530b67a2a9971e58fb28393ad2c02e
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-01 00:07:01 +0100

    area del

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,8 +6,8 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """square class child class of Rectangle"""
     def __init__(self, size):
         """init  square"""
         super().__init__(size, size)
         self.__size = size
-        self.area()
