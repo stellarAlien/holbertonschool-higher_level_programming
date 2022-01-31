e525fb2da27ac281ac5e2874269ad9881a5e0d04
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-31 23:24:35 +0100

    msg error

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,6 +17,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         if(not isinstance(value, int)):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{:} must be an integer".format(name))
         elif(value <= 0):
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{:} must be an integer".format(name))
