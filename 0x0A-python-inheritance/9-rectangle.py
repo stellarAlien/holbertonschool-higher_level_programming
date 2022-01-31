5ae89c6caeb4d3e1fdfde2370c74ee4d20303015
root <root@LAPTOP-8L71RL06.localdomain>
2022-01-31 23:35:49 +0100

    9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -10,13 +10,15 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """class Rectangle"""
     def __init__(self, width, height):
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
     def area(self):
         """calculate area"""
         return self.__width * self.__height
 
     def __str__(self):
-        """print dimensions of method"""
+        """print dimensions of instance"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
