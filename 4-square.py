2f458ca854605eb3a0361f61a0cc94d07ced9c61
root <root@LAPTOP-8L71RL06.localdomain>
2022-02-03 22:24:06 +0100

    4

--- 4-square.py
+++ 4-square.py
@@ -8,6 +8,7 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
 
+    @property
     def size(self, value):
         if(type(value) != int):
             raise TypeError("size must be an integer")
@@ -15,7 +16,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    @property
     def size(self):
         """returns size"""
         return self.__size
