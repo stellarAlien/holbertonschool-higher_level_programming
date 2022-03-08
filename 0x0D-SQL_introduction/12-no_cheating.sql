ccc7de5144a345d7012def4978b91ce1c11d08e2
stellarAlien <saifgaida@gmail.com>
2022-03-08 23:19:54 +0100

    12

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,4 @@
--- update row by name value
---query to update bob's score
-UPDATE second_table SET score = 10 WHERE name = "Bob";
+-- update row by name value; query to update bob's score
+UPDATE second_table
+SET score = 10 
+WHERE name = "Bob";
