2cc7eb263c3643f557a84eeb2a5c335c38263852
stellarAlien <saifgaida@gmail.com>
2022-03-08 23:44:09 +0100

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --  a script that lists all records of the table second_table of the database hbtn_0c 
-SELECT score, name from second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 ORDER BY score DESC;
