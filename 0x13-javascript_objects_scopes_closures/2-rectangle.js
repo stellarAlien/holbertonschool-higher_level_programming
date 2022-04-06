45879c711fa935c8033afa3c48e7287dc93a4159
stellarAlien <saifgaida@gmail.com>
2022-04-06 11:00:24 +0100

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(h) || isNaN(w)) { return; }
-    this.height = h;
     this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
