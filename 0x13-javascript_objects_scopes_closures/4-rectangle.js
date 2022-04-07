ed596f03559fd6e810941dd016cc32c6cffd0e21
stellarAlien <saifgaida@gmail.com>
2022-04-07 12:57:56 +0100

    4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -5,28 +5,27 @@ class Rectangle {
     this.height = h;
     this.width = w;
   }
-
+  double () {
+    for (let i = 0; i < this.height * 2; i++) {
+      for (let j = 0; j < this.width * 2; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+    return;
+  }
+  rotate () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+    return;
+  }
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      process.stdout.write('\n');
-    }
-  }
-
-  rotate () {
-    for (let i = 0; i <= this.width; i++) {
-      for (let j = 0; j <= this.height; j++) {
-        process.stdout.write('X');
-      }
-      process.stdout.write('\n');
-    }
-  }
-
-  double () {
-    for (let i = 0; i <= this.height * 2; i++) {
-      for (let j = 0; j <= this.width * 2; j++) {
         process.stdout.write('X');
       }
       process.stdout.write('\n');
