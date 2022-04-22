6883e7d976cddae05fc813450f5bd7c978bf33fa
stellarAlien <saifgaida@gmail.com>
2022-04-22 18:18:07 +0100

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,11 @@
 fetch status from a link
 """
 import urllib.request
+
 if __name__ == "__main__":
-	import urllib.response
-	with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+	with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
 		r = response.read()
-		print("Body response:")
-		print("	- type: {:}".format(type(r)))
-		print("	- content: {:}".format(r))
-		print("	- utf8 content: OK" if r.decode('utf8') else "")
+	print("Body response:")
+	print("    - type: {:}".format(type(r)))
+	print("    - content: {:}".format(r))
+	print("    - utf8 content: {:}".format(r.decode('utf8')))
