8c64f2b5f0df24ef1fe317a004b38218bc67d682
stellarAlien <saifgaida@gmail.com>
2022-04-26 12:59:09 +0100

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         r = response.read()
     print("Body response:")
-    print("	- type {:}".format(type(r)))
-    print("	- content: {:}".format(r))
-    print("	- utf8 content: {:}".format(r.decode('utf8')))
+    print("    - type: {:}".format(type(r)))
+    print("    - content: {:}".format(r))
+    print("    - utf8 content: {:}".format(r.decode('utf8')))
