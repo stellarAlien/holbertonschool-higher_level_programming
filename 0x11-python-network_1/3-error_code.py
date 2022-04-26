5b05613ae05e300ca4372cd1601987a39fa9692f
stellarAlien <saifgaida@gmail.com>
2022-04-26 13:02:10 +0100

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,15 +3,13 @@
  takes in a URL, sends a request to the URL and displays
  the body of the response
 """
-import urllib
 from sys import argv
-import urllib.error
-import urllib.request
+from urllib import request, error
 
 if "__name__" == "__main__":
     url = argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
+        with request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
+    except error.HTTPError as e:
         print("Error code: {}".format(e.code))
